7126c588b3afd7d2e10af6bface96300e93c5362
Jeremy <jeremy9451.lmd@gmail.com>
2022-08-09 01:24:52 -0500

    Agregue cambios en tallerEnPareja, Taller 1 y Taller 2

--- Taller 1/js/javaScript.js
+++ Taller 1/js/javaScript.js
@@ -1,1 +1,18 @@
-f
+
+const calcular = () => {
+    var x = document.getElementById('formX').value;
+    var y = document.getElementById('formY').value;
+    
+    
+    if (x == -6 || y == -5) {
+         document.write("es una operacion da una indeterminación");
+    }
+    else {
+        // resultado = (3+x)*(4+5) / (x+6)*(y+5);
+        var resultado = y+x;   
+        document.write(resultado);
+    }
+    
+}
+
+
