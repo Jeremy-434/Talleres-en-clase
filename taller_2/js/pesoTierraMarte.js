3a0675c069bc60f8f3fc7017546298b8d90f44ab
jeremy <jeremy9451.lmd@gmail.com>
2022-08-16 09:40:13 -0500

    Agregue el QUIZ del dia 16/08/22 pesoTierraMarte.js

---
+++ taller_2/js/pesoTierraMarte.js
@@ -0,0 +1,6 @@
+var masa = 50;
+
+var pesoTierra = masa * 9.81;
+var pesoMarte = masa * 3.711;
+
+console.log(`Su peso segun su masa ${masa}kg en la Tierra es: ${pesoTierra.toFixed(2)} y en Marte es: ${pesoMarte.toFixed(2)}`);
